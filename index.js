94b5c873e4ddd9c8ed40952836137ea3ede3ec85
bgamut <bgamut@gmail.com>
2018-08-20 11:18:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ fs.readFile(file,function(e,data){
 
 })
 */
+/*
 var WaveFile=require("wavefile")
 var fs= require("fs");
 var ooura = require('ooura');
@@ -95,6 +96,76 @@ fs.readFile("test.wav",function(e,data){
     var new_wav=new WaveFile();
     "new_wav.fromScratch(2,41100,'24',newLeft,newRight);"
     new_wav.fromScratch(2,sampleRate,'16',samples,{'container':'RIFF'});
+    
+    
+    fs.writeFileSync('master.wav',new_wav.toBuffer());
+})
+*/
+var fs = require('fs');
+var path = require('path');
+var WavDecoder = require('wav-decoder');
+var WavFile = require('wavefile');
+//var ooura = require('ooura');
+//var FFT = require ('fft');
+//var FFT = require('fft.js');
+var lib = require('ml-fft');
+var FFT = lib.FFT;
+var FFTUtils = lib.FFTUtils;
+var readFile = (filepath)=>{
+    return new Promise((resolve, reject)=>{
+        fs.readFile(filepath,(err,buffer)=>{
+            if(err){
+                return reject(err);
+            }
+            return resolve(buffer);
+        })
+    })
+}
+// relative path to the .wav file
+var wavPath = path.join(__dirname,'masteringjavascriptnode','master.wav')
+
+
+
+
+readFile(wavPath).then((buffer)=>{
+    return WavDecoder.decode(buffer);
+}).then (function(audioData){
+    //console.log(audioData.channelData[0]);// returns float 32 array
+    
+    //the following number needs to adjust according to the length of the array or we need to change the length of the original array
+    var bins=4;
+    /*
+    var f =new FFT(bins);
+    
+    var compLeft=f.toComplexArray(left);
+    var fftLeft = f.createComplexArray();
+    var ifftLeft = f.createComplexArray();
+    f.transform(fftLeft,compLeft);
+    */
+   var left= audioData.channelData[0];
+   FFT.init(bins);
+   var re = new Float32Array(bins);
+   var im = new Float32Array(bins);
+   for (var i = 0; i<left.length; i++){
+       re[i]=left[i]
+   }
+   im.fill(0);
+   FFT.fft(re,im);
+   console.log(left);
+   console.log(re);
+   FFT.ifft(re,im);
+   console.log(re);
+    //do something with the complex array fftLeft
+    /*
+    f.inverseTransform(ifftLeft,fftLeft)
+    var outLeft = f.fromComplexArray(ifftLeft);
+    
+    */
+    
+    //console.log(outLeft)
+    var new_wav=new WaveFile();
+    // the following needs to be changed
+    new_wav.fromScratch(2,audioData.sampleRate,'32',[left,right],{'container':'RIFF'});
     
     
     fs.writeFileSync('master.wav',new_wav.toBuffer());
