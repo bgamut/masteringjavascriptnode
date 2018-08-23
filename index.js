52427bbed470c063e465594d0870c6bdd8df64e3
bgamut <bgamut@gmail.com>
2018-08-24 07:12:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,16 @@ var readFile = (filepath)=>{
     })
 }
 // relative path to the .wav files
-var rwavPath = path.join(__dirname,'masteringjavascriptnode','reference.wav')
-var swavPath = path.join(__dirname,'masteringjavascriptnode','master.wav')
+var rWavPath = path.join(__dirname,'masteringjavascriptnode','reference.wav')
+var sWavPath = path.join(__dirname,'masteringjavascriptnode','master.wav')
 
-var refTable = (rWavPath)=>{
-    readFile(swavPath).then((buffer)=>{
+var table = (rWavPath)=>{
+    readFile(rWavPath).then((buffer)=>{
         return WavDecoder.decode(buffer);
     }).then (function(audioData){
         var bins = 1024
         var left = audioData.channelData[0];
         var right = audioData.channelData[1];
-        
         var sampleRate = audioData.sampleRate;
         var truncatedLength = left.length%bins*bins;
         var mono = new Float32Array(truncatedLength);
@@ -34,68 +33,108 @@ var refTable = (rWavPath)=>{
 
         for (var i =0; i<truncatedLength; i++){
             mono[i] = (left[i]+right[i])/2;
-            side[i] = left[i]-mono
+            side[i] = left[i]-mono;
         }
         var iterations = truncatedLength/bins;
-        //parseFloat() & .toString is needed to add the number to proper json (I think)
-        //e.g. row.mean=(parseFloat(row.mean)+2).toString()
-        //update::changed from var to function for prototype creation 
+
+        function savedFFTRow(){
+            this.mono = new Float32Array(bins);
+            this.side = new Float32Array(bins);
+            this.monoCos = new Float32Array(bins);
+            this.monoSin = new Float32Array(bins);
+            this.sideCos = new Float32Array(bins);
+            this.sideSin = new Float32Array(bins);
+            this.mono.fill(0);
+            this.side.fill(0);
+            this.monoCos.fill(0);
+            this.monoSin.fill(0);
+            this.sideSin.fill(0);
+            this.sideCos.fill(0);
+        };
+        
         function row(){
             this.monoMean = 0;
             this.monoSD = 0;
-            //this.monoRatio = 0;
             this.sideMean = 0;
             this.sideSD = 0;
-            //this.sideRatio = 0;
+            this.savedFFT = new Array(iterations);
         }
-        //var tableFull = new Float32Array[left[truncatedLength]]
-        var table = new Array(bins)
+        var t = new Array(bins)
         for (var i =0; i<bins; i++){
-            table[i]=new row;
+            t[i]=new row;
+            for (var j=0; j<iterations; j++){
+                t[i].savedFFT[j]=new savedFFTRow;
+            }
         }
+        
         var reMono = new Float32Array(bins);
         var imMono = new Float32Array(bins);
         var reSide = new Float32Array(bins);
         var imSide = new Float32Array(bins);
+        var reMonoCos = new Float32Array(bins);
+        var imMonoOne = new Float32Array(bins);
+        var reMonoSin = new Float32Array(bins);
+        var imMonoTwo = new Float32Array(bins);
+        var reSideCos = new Float32Array(bins);
+        var imSideOne = new Float32Array(bins);
+        var reSideSin = new Float32Array(bins);
+        var imSideTwo = new Float32Array(bins);
         imMono.fill(0);
         imSide.fill(0);
+        imMonoOne.fill(0);
+        imMonoTwo.fill(0);
+        imSideOne.fill(0);
+        imSideTwo.fill(0);
+
         FFT.init(bins);
-        var savedFFT = new Array(iterations);
-        function savedFFTRow(){
-            this.mono = new Float32Array(bins);
-            this.side= new Float32Array(bins);
-        }
+        
         // collecting mean value for middle and side
         for (var i = 0; i<iterations; i++){
-            savedFFT[i]=new savedFFTRow;
+            
             for (var j = 0; j<bins; j++){
-                reMono[j]=mono[bins*i+j]
-                reSide[j]=side[bins*i+j]
+                reMono=mono[bins*i+j];
+                reSide=side[bins*i+j];
+                reMonoCos[j]=Math.cos(j*Math.PI()/2)*mono[bins*i+j];
+                reMonoSin[j]=Math.sin(j*Math.PI()/2)*mono[bins*i+j];
+                reSideCos[j]=Math.cos(j*Math.PI()/2)*side[bins*i+j];
+                reSideSin[j]=Math.sin(j*Math.PI()/2)*side[bins*i+j];
+                
             }
             FFT.fft(reMono,imMono);
             FFT.fft(reSide,imSide);
+            FFT.fft(reMonoCos,imMonoOne);
+            FFT.fft(reMonoSin,imMonoTwo);
+            FFT.fft(reSideCos,imSideOne);
+            FFT.fft(reSideSin,imSideTwo);
             //accumulate mean per bin per iterations and get over all mean in the end
             for (var k = 0; k<bins; k++){
-                savedFFT[i].mono[k]=reMono[k]
-                savedFFT[i].side[k]=reSide[k]
-                table[k].monoMean+=reMono[k]/iterations
-                table[k].sideMean+=reSide[k]/iterations
+                t.savedFFT[i].mono[k]=reMono[k];
+                t.savedFFT[i].side[k]=reSide[k];
+                t.savedFFT[i].monoCos[k]=reMonoCos[k]
+                t.savedFFT[i].monoSin[k]=reMonoSin[k]
+                t.savedFFT[i].sideCos[k]=reSideCos[k]
+                t.savedFFT[i].sideSin[k]=reSideSin[k]
+                t[k].monoMean+=reMono[k]/iterations
+                t[k].sideMean+=reSide[k]/iterations
             }
         }
-        
         // collecting standard deviation value for middle and side
         for (var i = 0; i<iterations; i++){
             for (var j = 0; j<bins; j++){
-                table[i].monoSD+=Math.abs(table[i].monoMean-savedFFT[i].mono[j])/iterations
-                table[i].sideSD+=Math.abs(table[i].sideMean-savedFFT[i].side[j])/iterations
+                t[i].monoSD+=Math.abs(t[i].monoMean-t[i].savedFFT[i].mono[j]/iterations)
+                t[i].sideSD+=Math.abs(t[i].sideMean-t[i].savedFFT[i].side[j]/iterations)
             }
         }
 
-    return table
+        return t
     })
 }
 
-readFile(swavPath).then((buffer)=>{
+
+
+
+
+readFile(sWavPath).then((buffer)=>{
     return WavDecoder.decode(buffer);
 }).then (function(audioData){
     //console.log(audioData.channelData[0]);// returns float 32 array
@@ -106,12 +145,16 @@ readFile(swavPath).then((buffer)=>{
     var left= audioData.channelData[0];
     var right= audioData.channelData[1];
     
+    
     FFT.init(bins);
     var originalLength =left.length;
     //var iterations = (originalLength/bins)*2-1;
     var iterations = (originalLength/bins);
     var originalSR=audioData.sampleRate;
     var remainderAdded = originalLength%bins+originalLength;
+    var mono = new Float32Array(remainderAdded);
+    var leftOnly = new Float32Array(remainderAdded);
+    var rightOnly = new Float32Array(remainderAdded);
     var reLeftOne = new Float32Array(bins);
     var reLeftTwo = new Float32Array(bins);
     var imLeftOne = new Float32Array(bins);
@@ -121,6 +164,9 @@ readFile(swavPath).then((buffer)=>{
     var outLeft = new Float32Array(originalLength);
     var outRight = new Float32Array(originalLength);
     var ampRatio = new Float32Array(remainerAdded);
+    mono.fill(0);
+    leftOnly.fill(0);
+    rightOnly.fill(0);
     imLeftOne.fill(0);
     imLeftTwo.fill(0);
     imRightOne.fill(0);
@@ -210,3 +256,4 @@ readFile(swavPath).then((buffer)=>{
     });
 })
 
+var referenceTable=table(rWavPath);
