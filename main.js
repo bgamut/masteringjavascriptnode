12054eaf4cebc7ad4977a1438288cdb991e6ab5f
bgamut <bgamut@gmail.com>
2018-11-10 15:20:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-const {BrowserWindow, app, Menu, Tray} = require('electron')
-
+const electron = require('electron')
+const {ipcMain,BrowserWindow, app, Menu, Tray} = electron
+var path = require('path')
 let appIcon = null
 
+
 app.on('ready', () => {
-  const win= new BrowserWindow({width:120,height:100})
+  
+  const {width,height} =  electron.screen.getPrimaryDisplay().workAreaSize
+  const win= new BrowserWindow({width:125,height:100, transparent:true, skipTaskebar:true, title:'squwbs',frame:true,x:width-125,y:0,appIcon:__dirname + '/static/tray_icon.png'})
   win.loadFile('index.html')
   const tray = new Tray( __dirname + '/static/tray_icon.png')
+
   tray.on('click',()=>{
     win.isVisible() ? win.hide() : win.show()
   })
   win.on('show',()=>{
-    tray.setHighlightMode('always')
+    tray.setHighlightMode('never')
+
   })
   win.on('hide',()=>{
     tray.setHighlightMode('never')
   })
 
 })
+
+
